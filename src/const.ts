7f73ab030dc352ee5eb0357a0940aadcd13da4e7
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-04-25 19:49:53 -0600

    build: desplegar a en vercel con las nuevas variables del back end

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,3 @@
 // Archivo centralizado para constantes globales
 
 export const BACKEND_URL = 'https://short-url-back-end.onrender.com';
-
-// Cambia el valor de BACKEND_URL aquí si tu backend cambia de dirección.
